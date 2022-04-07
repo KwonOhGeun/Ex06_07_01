7eeb6da1eff617d8089a15f5317b5a1a4eee77c5
KwonOhGeun <ohgeun72@naver.com>
2022-04-07 09:59:58 +0900

    Initial commit

---
+++ src/main/java/Ex06_07_01.java
@@ -0,0 +1,15 @@
+public class Ex06_07_01 {
+    public static void main(String[] args) {
+        //5! 계산하기 for 문으로
+
+        int f = 1;
+
+        for ( int i = 5; i > 0; i--){
+            f *= i;
+            System.out.println("중간 계산 값 : " + f);
+
+
+        }
+        System.out.printf("5!의 값 : " +f );
+    }
+}
